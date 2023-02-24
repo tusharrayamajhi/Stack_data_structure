312379f2f6cff7dc9ffdfeaa2855df08ad8a07b2
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2023-02-24 17:00:33 +0545

    Create Stack_using_ArrayList.java

---
+++ Stack_using_ArrayList.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+public class Stack_using_ArrayList {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        //isempty
+         static boolean isempty(){
+            return list.size() == 0;
+        }
+        //push
+        public static void push(int data) {
+            list.add(data);
+        }
+        //pop
+        public static int pop(){
+            if(isempty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+        //peek
+        public static int peek(){
+            if(isempty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isempty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
